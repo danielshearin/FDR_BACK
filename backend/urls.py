3e7a4b5356e96826af3e5055691eb3f14635a1fa
danielshearin <danielshearin@gmail.com>
2021-10-11 11:31:17 -0400

    last cleanup

--- backend/urls.py
+++ backend/urls.py
@@ -22,7 +22,6 @@ from django.views.generic import TemplateView
 router = routers.DefaultRouter()
 router.register('restaurants', views.RestaurantView, 'restaurant')
 router.register('menuitems', views.MenuItemView, 'menuitem')
-# router.register('todos', views.TodoView, 'todo')
 
 
 urlpatterns = [
